95c46f875d1f3e77838745ca58dbbc481109280c
S.Agalya <agalyaishere@gmail.com>
2024-02-21 22:17:38 +0530

    first commit

---
+++ celsipus.test.js
@@ -0,0 +1,13 @@
+const assert=require("assert")
+
+function celsious(c){
+    var f=(c*(9/5)+32)    //formula to convert celsius to fahrenheit
+return f
+}
+
+describe("celsious to fahrenheit conversion", function(){
+    it("check the fahrenheit",function(){
+        var result=celsious(6)
+        assert.equal(result,"42.8")
+    })
+})
